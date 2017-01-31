3cc8206df7e13c45d187f7407b669829c230f8aa
JP Colomer <jpcolomer@thenumber.com>
2017-01-31 14:53:54 -0500

    Uses valid indices.create syntax

--- targets/elastic.js
+++ targets/elastic.js
@@ -43,12 +43,16 @@ module.exports = (stream,argv,schema) => {
       // Try creating the index with settings and mappings (if defined)
       if (settings)
         ['provided_name','creation_date','uuid', 'version'].forEach(f => delete settings.index[f]);
+      if (mapping) {
+        mapping[argv.target_indextype] = mapping[argv.source_indextype]
+        delete mapping[argv.source_indextype]
+      }
+
       return client.indices.create({
         index: argv.target_index,
-        type: argv.target_indextype,
         body: {
           settings: settings,
-          mapping: mapping 
+          mappings: mapping
         }
       })
       .then(
