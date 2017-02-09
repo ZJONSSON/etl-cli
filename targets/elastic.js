dc8fc7f8d8efef4894cb40f42ec50caf6d5cf8cf
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-02-09 14:42:29 -0500

    Fix: problem if source other than elastic This code was possibly not required

--- targets/elastic.js
+++ targets/elastic.js
@@ -43,10 +43,6 @@ module.exports = (stream,argv,schema) => {
       // Try creating the index with settings and mappings (if defined)
       if (settings && settings.index)
         ['provided_name','creation_date','uuid', 'version'].forEach(f => delete settings.index[f]);
-      if (mapping && argv.target_indextype !== argv.source_indextype) {
-        mapping[argv.target_indextype] = mapping[argv.source_indextype];
-        delete mapping[argv.source_indextype];
-      }
 
       return client.indices.create({
         index: argv.target_index,
