8960f12d6110a1cc658fd7833ed8fc9067a054fd
JP Colomer <jpcolomer@thenumber.com>
2017-02-14 13:24:41 -0500

    Uses mysql schema to derive elastic mapping

--- sources/mysql.js
+++ sources/mysql.js
@@ -17,8 +17,42 @@ module.exports = argv => {
 
   key_query = mysql.format('SHOW KEYS FROM ?? WHERE Key_name="PRIMARY"', [argv.source_table]);
   query = argv.source_query || mysql.format('SELECT * FROM ??',[argv.source_table]);
+  schema_query = argv.schema_query || mysql.format('SELECT COLUMN_NAME, COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?',[argv.source_database, argv.source_table]);
+
+  const sqlTypeToElastic = (type) => {
+    type = type.toLowerCase();
+    if (type.includes("char"))
+      return {
+        type: "text",
+        fields: {
+          keyword: {
+            type: "keyword",
+            ignore_above: 256
+          }
+        }
+      };
+    if (type.includes("int"))
+      return {type: "long"};
+    if (type.includes("float"))
+      return {type: "float"};
+    if (type.includes("date"))
+      return {type: "date", ignore_malformed: true};
+    return type;
+  };
+
+  const sqlToElasticSchema = function(q) {
+    return p.query(q).then(columns => {
+      return Promise.resolve(columns.reduce((r,c) => {
+        r[c.COLUMN_NAME] = sqlTypeToElastic(c.COLUMN_TYPE);
+        return r;
+      }, {}))
+    }).then(d => ({properties: d}))
+  };
 
   return {
+    elastic: {
+      mapping: () => sqlToElasticSchema(schema_query)
+    },
     recordCount : () => p.query(mysql.format('SELECT COUNT(*) AS recordCount FROM ??',[argv.source_table])).then(d => d[0].recordCount),
     stream : () => etl.toStream(function(){
       return p.query(key_query)
