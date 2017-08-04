9621257380db29fba6970b9aa2c6d54fd0428512
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-08-04 17:53:46 -0400

    Add custom json_indent (for debugging json output)

--- targets/json.js
+++ targets/json.js
@@ -1,6 +1,6 @@
 const etl = require('etl');
 
 module.exports = function(stream,argv) {
-  return stream.pipe(etl.stringify(0,null,true))
+  return stream.pipe(etl.stringify(argv.json_indent || 0,null,true))
     .pipe(argv.source === 'screen' ? etl.map(d => console.log(d)) : etl.toFile(argv.dest));
 };
