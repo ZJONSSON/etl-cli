4011b132883d19da95984626e4e5e0818bb221a4
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-08-22 18:06:41 -0400

    Add `source_query` to mongo source Performs a safe eval on the source_query to generate a query object.   Safe eval here allows for "new Date(...)"

--- sources/mongo.js
+++ sources/mongo.js
@@ -2,17 +2,23 @@ const etl = require('etl');
 
 module.exports = argv => {
   const mongodb = require('mongodb');
+  let query = undefined;
+  if (argv.source_query) {
+    const vm = require('vm');
+    query = vm.runInNewContext(`ret = ${argv.source_query}`);
+  }
+
   ['source_uri','source_collection'].forEach(key => { if(!argv[key]) throw `${key} missing`;});
 
   const db = mongodb.connect(argv.source_uri);
 
   return {
     recordCount : () => db.then(db => 
-      db.collection(argv.source_collection).count(argv.source_query)
+      db.collection(argv.source_collection).count(query)
     ),
     stream: () => etl.toStream(db.then(db =>
       db.collection(argv.source_collection)
-        .find(argv.source_query)
+        .find(query)
         .pipe(etl.map(d => {
           d._id = String(d._id);
           return d;
