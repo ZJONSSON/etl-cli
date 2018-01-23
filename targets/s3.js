14825acb7f98bca46388178929724487b59e9ab4
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2018-01-23 13:06:31 -0500

    Only stringify s3 for objects (strings and buffers should passthrough)

--- targets/s3.js
+++ targets/s3.js
@@ -20,7 +20,13 @@ module.exports = function(stream,argv) {
   const out = etl.map();
   
   stream
-    .pipe(etl.stringify(0,null,true))
+    .pipe(function(d) {
+      if (d instanceof Object && !(d instanceof Buffer)) {
+        return JSON.stringify(d)+'\n';
+      } else {
+        return d;
+      }
+    })
     .pipe(upload)
     .on('error', e=> out.emit('error',e))
     .on('uploaded',() =>  out.end());
