42c1ba2496ba616ca2afac3eb58e21b3cce2a1e9
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2018-01-30 16:57:06 -0500

    Allow https paths

--- sources/getFile.js
+++ sources/getFile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = source => {
-  if (/https/.exec(source))
+  if (/https?:/.exec(source))
     return require('request').get(source);
   else if (source === 'stdin')
     return process.stdin;
