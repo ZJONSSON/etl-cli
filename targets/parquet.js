2395853a57555552f9334913afdb7a8694c51599
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2019-10-05 21:40:55 -0400

    Add parquet source/target

---
+++ targets/parquet.js
@@ -0,0 +1,30 @@
+const etl = require('etl');
+const parquet = require('parquetjs-lite');
+
+module.exports = (stream, argv, schema) => {
+  schema = schema && schema.parquet && new parquet.ParquetSchema(schema.parquet);
+  if (!schema) throw 'schema missing';
+  const out = etl.map();
+
+  parquet.ParquetWriter.openStream(schema, out).then(writer => {
+    if (argv.rowGroupSize) writer.setRowGroupSize(argv.rowGroupSize);
+    stream.pipe(etl.map(async d => {
+      try {
+        await writer.appendRow(d);
+      } catch(e) {
+        out.emit('error',e);
+      }
+    }))
+    .on('finish', async () => {
+      await writer.close();
+      out.end();
+    });
+  });
+
+  return out
+    .pipe(etl.map(d => {
+      if (typeof d === 'function') return;
+      return d;
+    }))
+    .pipe(etl.toFile(argv.dest)); 
+};
