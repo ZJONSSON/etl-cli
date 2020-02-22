cade298d78c13c511365ebfcca7d1a208bdd2575
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2020-02-22 10:08:07 -0500

    Athena fix backpressure

--- sources/athena.js
+++ sources/athena.js
@@ -15,55 +15,65 @@ module.exports = argv => {
   const athena = new AWS.Athena(argv.source_config || argv);
   const s3 = new AWS.S3(argv.source_config || argv);
 
-  return () => etl.toStream(QueryString.split(/;\s*\n/g))
-    .pipe(etl.map(async function(QueryString) {
-      if (!QueryString.trim().length) return;
+  return () => {
+    const out = etl.map(null, {keepAlive: true});
 
-      let version = argv.version && argv.version.includes('rand') ? Math.random() : argv.version;
-      const ClientRequestToken = crypto.createHash('md5').update(QueryString+version).digest('hex');
+    etl.toStream(QueryString.split(/;[\s\n]*/g))
+      .pipe(etl.map(async function(QueryString) {
+        if (!QueryString.trim().length) return;
 
-      if (argv.verbose) console.log(`Executing query ${JSON.stringify(QueryString.slice(0,70))}`);
-      
-      const params = {
-        QueryString,
-        ClientRequestToken,
-        QueryExecutionContext: { Database },
-        ResultConfiguration: { OutputLocation },
-        WorkGroup: 'primary'
-      };
+        let version = argv.version && argv.version.includes('rand') ? Math.random() : argv.version;
+        const ClientRequestToken = crypto.createHash('md5').update(QueryString+version).digest('hex');
 
-      const res = await athena.startQueryExecution(params).promise();
-      const QueryExecutionId = res.QueryExecutionId;
+        if (argv.verbose) console.log(`Executing query ${JSON.stringify(QueryString.slice(0,70))}`);
+        
+        const params = {
+          QueryString,
+          ClientRequestToken,
+          QueryExecutionContext: { Database },
+          ResultConfiguration: { OutputLocation },
+          WorkGroup: 'primary'
+        };
 
-      const fetch = async() => {
-        let execution = await athena.getQueryExecution({QueryExecutionId}).promise();
-        execution = execution.QueryExecution;
-        const state = execution.Status.State;
-        if (/QUEUED|RUNNING/.test(state))
-          return Promise.delay(250).then(() => fetch());
-        else if (state == 'FAILED')
-          throw execution.Status.StateChangeReason.message || execution.Status.StateChangeReason;
-        else if (state == 'SUCCEEDED') {
-          const [, Bucket, Key] = /s3:\/\/([^/]+)\/(.*)/.exec(execution.ResultConfiguration.OutputLocation);
-          return s3.getObject({Bucket, Key})
-            .createReadStream()
-            .pipe(etl.csv())
-            // TODO nested fields need to be parsed into JSON
-            .pipe(etl.map(d => {
-              Object.keys(d).forEach(key => {
-                // Try decoding structured output
-                if (d[key][0] == '[' || d[key][0] == '{') {
-                  try {
-                    d[key] = athenaParser.parse(d[key]);
-                  } catch(e) {}
-                }
-              });
-              this.push(d);
-            }))
-            .promise();
-        }
-      };
+        const res = await athena.startQueryExecution(params).promise();
+        const QueryExecutionId = res.QueryExecutionId;
 
-      await fetch();
-    })); 
+        const fetch = async() => {
+          let execution = await athena.getQueryExecution({QueryExecutionId}).promise();
+          execution = execution.QueryExecution;
+          const state = execution.Status.State;
+          if (/QUEUED|RUNNING/.test(state))
+            return Promise.delay(250).then(() => fetch());
+          else if (state == 'FAILED')
+            throw execution.Status.StateChangeReason.message || execution.Status.StateChangeReason;
+          else if (state == 'SUCCEEDED') {
+            const [, Bucket, Key] = /s3:\/\/([^/]+)\/(.*)/.exec(execution.ResultConfiguration.OutputLocation);
+            const stream = s3.getObject({Bucket, Key})
+              .createReadStream()
+              .pipe(etl.csv())
+              // TODO nested fields need to be parsed into JSON
+              .pipe(etl.map(d => {
+
+                Object.keys(d).forEach(key => {
+                  // Try decoding structured output
+                  if (d[key][0] == '[' || d[key][0] == '{') {
+                    try {
+                      d[key] = athenaParser.parse(d[key]);
+                    } catch(e) {}
+                  }
+                });
+                return d;
+              }));
+
+            stream.pipe(out);
+            return new Promise( (resolve, reject) => stream.on('finish',resolve).on('error',reject));
+          }
+        };
+
+        await fetch();
+      }))
+      .on('finish', () => out.end())
+      .on('error', e => out.error(e));
+    return out;
+  };
 };
