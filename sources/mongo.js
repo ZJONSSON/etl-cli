e26d471c0b61407e681b018764e435fa6da1e337
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2020-02-25 15:59:51 -0500

    add ability to specify mongo fields

--- sources/mongo.js
+++ sources/mongo.js
@@ -8,13 +8,15 @@ module.exports = argv => {
   const db = mongodb.connect(argv.source_uri);
   const query = argv.source_query;
 
+  const fields = argv.fields ? argv.fields.split(',').reduce( (p,key) => { p[key] = 1; return p;},{}) : undefined;
+
   return {
     recordCount : () => db.then(db => 
       db.collection(argv.source_collection).count(query)
     ),
     stream: () => etl.toStream(db.then(db =>
       db.collection(argv.source_collection)
-        .find(query)
+        .find(query, fields)
         .pipe(etl.map(d => {
           d._id = String(d._id);
           return d;
