cf2666b5b75fde1733185a4aa9abcd230878465e
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2020-12-26 14:21:54 -0500

    Add node fetch and proxy agent

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 const path = require('path');
 const request = require('request');
+const fetch = require('node-fetch');
+const HttpsProxyAgent = require('https-proxy-agent');
 const minimist = require('minimist');
 const nconf = require('nconf')
   .file({file: process.env.ETL_CONFIG || path.resolve(process.env.HOME || process.env.USERPROFILE,'.etlconfig.json')});
@@ -19,11 +21,23 @@ if (!module.parents) {
   if (argv.proxy && nconf.get(argv.proxy))
     argv.proxy = nconf.get(argv.proxy);
 
+  argv.userAgent = argv.userAgent || nconf.get('user-agent') || 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36';
+
   // expose nconf in the argv
   argv.nconf = nconf;
 
   // getProxy returns a new proxy string where {{random}} has been replaced with a random number
   argv.getProxy = () =>  argv.proxy ? argv.proxy.replace('{{random}}',String(Math.random())) : undefined;
+
+  // inject node-fetch with proxy injection
+  argv.fetch = async (url, opt) => {
+    opt = opt || {};
+    if (opt.proxy && !argv.proxy) throw '--proxy missing';
+    opt.headers = opt.headers || {};
+    opt.headers['user-agent'] = opt.headers['user-agent'] || argv.userAgent;
+    if (argv.proxy) opt = Object.assign({agent: new HttpsProxyAgent(argv.getProxy())},opt);
+    return fetch(url, opt);
+  }
 
   // Include default request / requestAsync that use proxy (if supplied) automatically
   argv.request = d => request(Object.assign({proxy: argv.getProxy()},d));
