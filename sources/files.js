6e1127f26ef627ae67db76815c39c38270bfbeda
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2021-01-31 19:19:14 -0500

    recursive-dir is promisified already

--- sources/files.js
+++ sources/files.js
@@ -1,7 +1,6 @@
 const etl = require('etl');
 const Promise = require('bluebird');
 const recursive = require('recursive-readdir');
-const recursiveAsync = Promise.promisify(recursive);
 const jsonSource = require('./json');
 const getFile = require('./getFile');
 
@@ -11,7 +10,7 @@ module.exports = function(argv) {
   const reFilter = RegExp(argv['filter-files']);
 
   return {
-    stream: () => etl.toStream(() => recursiveAsync(source_dir))
+    stream: () => etl.toStream(() => recursive(source_dir))
       .pipe(etl.map(filename => {
         if (reFilter.exec(filename)) return {
           filename,
