4bebe557f96e2d937b8adf823e4778f4d63f91aa
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2021-01-31 19:20:54 -0500

    Don't include the selected directory in the filename

--- sources/files.js
+++ sources/files.js
@@ -8,12 +8,13 @@ module.exports = function(argv) {
   const source_dir = argv.source_dir || argv.source_collection;
   if (!source_dir) throw 'Not source_dir';
   const reFilter = RegExp(argv['filter-files']);
+  const reSourceDir = new RegExp(`^${source_dir}/`);
 
   return {
     stream: () => etl.toStream(() => recursive(source_dir))
       .pipe(etl.map(filename => {
         if (reFilter.exec(filename)) return {
-          filename,
+          filename: filename.replace(reSourceDir,''),
           body: () => /.json$/.test(filename) ? jsonSource({source: filename}) : getFile(filename)
         };
       }))
