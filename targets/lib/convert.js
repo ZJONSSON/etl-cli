b851b3ab53efc867366881b482bf34e164b75df2
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2021-01-31 19:13:41 -0500

    Don't convert to json if buffer

--- targets/lib/convert.js
+++ targets/lib/convert.js
@@ -1,8 +1,11 @@
 const csv = require('./csv');
 const json = require('./json');
+const etl = require('etl');
 
 module.exports = (stream, filename, argv) => {
-  if (filename.endsWith('.csv')) stream = csv(stream, argv);
-  else if (!argv.target_raw) stream = json(stream, argv);
-  return stream
+  if (filename.endsWith('.csv')) return csv(stream, argv);
+  return stream.pipe(etl.map(d => {
+    if (typeof d == 'object' && !Buffer.isBuffer(d)) return JSON.stringify(d, null,  argv.json_indent || 0)+'\n';
+    return d;
+  }));
 }
