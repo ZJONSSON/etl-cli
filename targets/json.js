533c677eef9b63707b35964b64c0fadbb7364558
zjonsson <ziggy.jonsson.nyc@gmail.com>
2022-11-28 09:34:01 -0500

    Add option json_collect to write a single json file (array) instead of multiline

--- targets/json.js
+++ targets/json.js
@@ -1,6 +1,10 @@
 const etl = require('etl');
 
 module.exports = function(stream,argv) {
+  if (argv.json_collect) {
+    // TODO stream the collect by adding brackets on both sides and append commas
+    stream = stream.pipe(etl.collect(Math.Inf));
+  }
   return stream.pipe(etl.stringify(argv.json_indent || 0,null,true))
     .pipe(argv.source === 'screen' ? etl.map(d => console.log(d)) : etl.toFile(argv.dest));
 };
