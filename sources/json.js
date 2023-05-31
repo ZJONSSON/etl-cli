ea7f962b6527a4b9c6c1bec71657b82fc2f6fba8
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2023-05-30 20:39:06 -0400

    if json_multiline === false then assume entire file is json

--- sources/json.js
+++ sources/json.js
@@ -2,14 +2,21 @@ const etl = require('etl');
 const getFile = require('./getFile');
 
 module.exports = function(argv) {
-    return () => getFile(argv.source)
-    .pipe(etl.split())
-    .pipe(etl.map(d => {
-      try {
-        const text = (d.text || d).replace(/,\s*$/,'');
-        return JSON.parse(text);
-      } catch(e) {
-        if (argv.strict_json) throw e;
-      }
-    }));
+  return () => {
+    let file = getFile(argv.source);
+    if (argv.json_multiline) {
+      const data = file.pipe(etl.map(d => d.toString())).promise()
+      return data.then(d => JSON.parse(d.join('')));
+    }
+    return file
+      .pipe(etl.split())
+      .pipe(etl.map(d => {
+        try {
+          const text = (d.text || d).replace(/,\s*$/,'');
+          return JSON.parse(text);
+        } catch(e) {
+          if (argv.strict_json) throw e;
+        }
+      }));
+  }
 };
