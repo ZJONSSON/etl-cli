7a5300a200bc8c73e15714e8f04f2377d7245f53
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-04-13 16:42:53 -0400

    hotfix

--- sources/files.js
+++ sources/files.js
@@ -13,7 +13,7 @@ module.exports = function(argv) {
     stream: () => etl.toStream(() => recursive(source_dir))
       .pipe(etl.map(filename => {
         if (reFilter.exec(filename)) return {
-          filename: filename.replace(reSourceDir,''),
+          filename: filename.replace(source_dir+'/',''),
           body: (raw) => {
             raw = raw || argv.source_raw;
             if (!raw && /.json$/.test(filename)) return jsonSource({...argv, source: filename, })();
