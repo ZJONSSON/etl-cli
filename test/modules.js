0b760c4ba9c06955289335dd436bf6ca3589943b
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-07-21 15:03:38 -0400

    Add module failure tests

---
+++ test/modules.js
@@ -0,0 +1,16 @@
+const tap = require('tap');
+const { cli } = require('./util');
+
+tap.test('modules', async t => {
+  t.test('broken js', async () => {
+    const cmd = `etl ${__dirname}/support/broken_js test --silent`;
+    t.rejects( cli(cmd), { message:'Broken JS file' });
+  });
+
+  t.test('broken ts', async () => {
+    const cmd = `etl ${__dirname}/support/broken_ts test --silent`;
+    t.rejects( cli(cmd).catch((e) => {
+      throw { message: e.message.split('\n')[0] };
+    }), { message:'⨯ Unable to compile TypeScript:' });
+  });
+});
